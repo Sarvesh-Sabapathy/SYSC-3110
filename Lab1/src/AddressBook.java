48903fef6f8ef08a56d107b2b590aeae3de26418
Sarvesh <otto.sarv@gmail.com>
2023-09-19 12:19:41 -0400

    Testing Git Commit

---
+++ Lab1/src/AddressBook.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+
+    private List<BuddyInfo> buddyList;
+
+    public AddressBook() {
+        buddyList = new ArrayList<>();
+    }
+    public void addBuddy(BuddyInfo buddy){}
+
+    public void removeBuddy(BuddyInfo buddy){}
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
